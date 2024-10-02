53fe6a1bc18637520723ae4a3fb183c32fa4f5c2
Enes KILIÇ <engineereneskilic@gmail.com>
2024-10-02 10:45:58 +0300

    casting

--- 1.week_3_Casting/Program.cs
+++ 1.week_3_Casting/Program.cs
@@ -6,7 +6,14 @@ namespace _1.week_3_Casting
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string userInput = "42";
+            int result = 0; // Dönüşüm başarısız olduğunda varsayılan değer
+
+            // Dönüşümü gerçekleştir
+            int.TryParse(userInput, out result);
+
+            // Dönüşüm sonucunu kontrol et
+            Console.WriteLine("Dönüşüm başarılı. Sonuç: " + result);
         }
     }
 }
