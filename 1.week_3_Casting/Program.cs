a8b4a63ab3d87b42f6fa1998ffbc78b9c3c756d1
Enes KILIÇ <engineereneskilic@gmail.com>
2024-10-02 15:03:56 +0300

    casting_finish

--- 1.week_3_Casting/Program.cs
+++ 1.week_3_Casting/Program.cs
@@ -12,11 +12,32 @@ namespace _1.week_3_Casting
 
             string userInput = "42";
             int result = 0; // Dönüşüm başarısız olduğunda varsayılan değer
-
+          
             int.TryParse(userInput, out result);
-            
-
             Console.WriteLine("Dönüşüm başarılı. Sonuç: " + result);
+
+
+            int num = 123;
+            double d= num; // implict casting
+                
+            double number= 123.5;
+
+            int number_int = (int)number;
+
+            Console.WriteLine(number_int);
+
+
+            string cnStrNum = "123";
+
+            int k=Convert.ToInt32(cnStrNum);
+
+            //try ve parse
+            int.Parse(cnStrNum);
+
+            bool result2 = int.TryParse(cnStrNum, out result);
+
+
+
         }
     }
 }
