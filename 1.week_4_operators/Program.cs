db6f1cb88932b370a82f00a41671ad4f2a12d0ad
Enes KILIÇ <engineereneskilic@gmail.com>
2024-10-02 17:43:23 +0300

    inputOutput

---
+++ 1.week_4_operators/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _1.week_4_operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 6;
+
+            int sum = a + b;
+            int diff = a - b;
+            int quatient = a / b;
+
+            Console.WriteLine(sum);
+
+            int remainder = b % a;
+
+            bool isEqual = a == b;
+            bool isNotEqual = a != b;
+
+            bool isGreaterThan = a > b;
+
+            bool isLessThan = a < b;
+
+            bool lessThanOrEqual = a <= b;
+
+            bool isGreaterThanOrEqual = a >= b;
+
+            bool result = (a > 0) && (b > 0);
+
+            bool result2 = a > 0 || b > 0;
+
+            bool notResult = !(a > b);
+
+            a = a + 5;
+            a += 10;
+        }
+    }
+}
---
+++ inputOutput/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace inputOutput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string greeting = "Merhaba Dünya";
+
+            Console.WriteLine(greeting);
+
+            Console.Write("Lütfen isminizi yazınız:");
+            string isim = Console.ReadLine();
+
+
+
+
+        }
+    }
+}
