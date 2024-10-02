7860c5a7e1ba8b550dd1683083ce9c8c9eab837f
Enes KILIÇ <engineereneskilic@gmail.com>
2024-10-02 13:21:19 +0300

    cast

--- 1.week_3_Casting/Program.cs
+++ 1.week_3_Casting/Program.cs
@@ -6,16 +6,17 @@ namespace _1.week_3_Casting
     {
         static void Main(string[] args)
         {
-,
-            
+
+
             // implicit casting küçük bir veri türünün daha büyük birüveri türüne dönüştürülmesi için kullanılır
 
             string userInput = "42";
             int result = 0; // Dönüşüm başarısız olduğunda varsayılan değer
 
             int.TryParse(userInput, out result);
-
+            
 
             Console.WriteLine("Dönüşüm başarılı. Sonuç: " + result);
         }
     }
+}
