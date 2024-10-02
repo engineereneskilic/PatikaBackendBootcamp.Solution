4d079c5be50249a07a9ec2039d401d7f1d73188c
Enes KILIÇ <engineereneskilic@gmail.com>
2024-10-02 22:28:51 +0300

    ControlStructuresOK

---
+++ 1.week_6_ControlStructuresAndLoops/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _1.week_6_ControlStructuresAndLoops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 10;
+            int number2 = -5;
+
+            if(number2>0)
+            {
+                Console.WriteLine("Bu sayı pozitif");
+            } else
+            {
+                Console.WriteLine("Bu sayı negatiftir");
+            }
+
+            if(number > 0)
+            {
+                if(number % 2 == 0)
+                {
+                    Console.WriteLine("Sayı çifttir");
+                }
+                else
+                {
+                    Console.WriteLine("Sayı çifttir");
+                }
+            }
+
+            int kullaniciYasi = 20;
+            
+            if(kullaniciYasi >= 18)
+            {
+                Console.WriteLine("Kullanıcı yetişkindir");
+            }
+            else
+            {
+                Console.WriteLine("kullanıcı reşit değildir");
+            }
+
+        }
+    }
+}
