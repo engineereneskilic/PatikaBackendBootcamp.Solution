c13b00372f401b50987ddef7427ed36383fffa5c
Enes KILIÇ <engineereneskilic@gmail.com>
2024-10-03 21:19:52 +0300

    sorular cozumu

---
+++ 1.week_PERSEMBE_s07/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _1.week_PERSEMBE_s07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            baslangic:
+            Console.WriteLine("Notunuz: ");
+            int not = Convert.ToInt32(Console.ReadLine());
+            string sonuc;
+            if( not < 0 || not > 100 )
+            {
+                goto baslangic;
+            }
+
+            if (not > 80)
+            {
+                sonuc = "Başarılı";
+            } else  if(not > 60)
+            {
+                sonuc = "Orta";
+            }
+            else
+            {
+                sonuc = "Başarısız";
+            }
+
+            Console.WriteLine("Sonuç: " + sonuc);
+
+        }
+    }
+}
---
+++ 1.week_PERSEMBE_s08/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _1.week_PERSEMBE_s08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ay no: ");
+            int ay = Convert.ToInt32(Console.ReadLine());
+
+            int adet;
+
+            switch (ay)
+            {
+                case 2:
+                    adet = 28;
+                    break;
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    adet = 31;
+                    break;
+                default:
+                    Console.WriteLine("Geçersiz bir ay numarası girdiniz !");
+                    Console.ReadKey();
+                    return;
+            }
+
+            Console.WriteLine("Gün adeti: " + adet);
+            Console.ReadKey();
+        }
+    }
+}
