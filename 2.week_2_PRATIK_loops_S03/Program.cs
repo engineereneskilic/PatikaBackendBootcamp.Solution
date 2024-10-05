983c860885dd137e0817ddafb2a0192e9ed3a63e
Enes KILIÇ <engineereneskilic@gmail.com>
2024-10-05 21:48:15 +0300

    s03

---
+++ 2.week_2_PRATIK_loops_S03/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _2.week_2_PRATIK_loops_S03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1 ile 20 arasındaki sayılar içinde sadece çift sayıları yazdırmak için bir for döngüsü kullanıyoruz
+            // Döngü 1'den başlayıp 20'ye kadar devam eder, ancak sadece çift sayıları ekrana yazdırır
+            for (int i = 1; i <= 20; i++)
+            {
+                // Eğer sayı 2'ye tam bölünebiliyorsa bu sayıdır
+                if (i % 2 == 0)
+                {
+                    // Çift sayıyı ekrana yazdırıyoruz
+                    Console.WriteLine(i);
+                }
+            }
+
+            // Programın kapanmaması için bir tuşa basılmasını bekliyoruz
+            Console.ReadKey();
+        }
+    }
+}
