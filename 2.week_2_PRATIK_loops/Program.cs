1d205bd2a908bf5fc8f217bcbc51dcc184548eb9
Enes KILIÇ <engineereneskilic@gmail.com>
2024-10-05 21:37:44 +0300

    soru 1 tamamlandı

---
+++ 2.week_2_PRATIK_loops/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _2.week_2_PRATIK_loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Mesajı yazdırmak için tekrar sayısını belirliyoruz
+            int tekrarSayisi = 10; // Mesajın 10 kez yazdırılacağını belirten değişken
+
+            // Döngü kullanarak mesajı 10 kez ekrana yazdırıyoruz
+            for (int i = 1; i <= tekrarSayisi; i++)
+            {
+                // Her döngüde mesajı ekrana yazdırıyoruz
+                Console.WriteLine("Kendime inanıyorum, ben bu yazılım işini hallederim!");
+            }
+
+            // Kullanıcı programı kapatmadan önce bir tuşa basmayı bekliyoruz
+            Console.ReadKey();
+        }
+    }
+}
