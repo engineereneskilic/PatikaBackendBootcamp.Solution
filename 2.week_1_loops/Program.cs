1f1d2994184448eb0f2e93161c125840f2ce9ac4
Enes KILIÇ <engineereneskilic@gmail.com>
2024-10-05 21:31:29 +0300

    faktoriyel

---
+++ 2.week_1_loops/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _2.week_1_loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                //Console.WriteLine(i);
+            }
+
+            for (int i = 10;i > 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+
+            for (int i = 10; i > 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+
+            int[] sayilar = { 1, 2, 3, 4, 5, 6 };
+
+            foreach (var item in sayilar)
+            {
+
+            }
+
+            int sayi = 5;
+            int faktoriyel = 1;
+
+            for(int i = 1; i <= sayi;i++)
+            {
+                faktoriyel *= i;
+            }
+
+            Console.WriteLine($"Faktoriye:" + faktoriyel);
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
