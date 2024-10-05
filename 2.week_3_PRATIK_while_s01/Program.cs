80dac660a5ba9a77e754b1bc50badc9c2970c501
Enes KILIÇ <engineereneskilic@gmail.com>
2024-10-05 22:42:33 +0300

    while pratik s01

---
+++ 2.week_3_PRATIK_while_s01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _2.week_3_PRATIK_while_s01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Mesajı kaç kez yazdıracağımızı belirleyen sayaç değişkeni
+            int sayac = 1;
+
+            // While döngüsü koşulu: sayaç 10'dan küçük veya eşit olduğu sürece döngü devam eder
+            while (sayac <= 10)
+            {
+                // Mesajı ekrana yazdır
+                Console.WriteLine("Kendime inanıyorum, ben bu yazılım işini hallederim!");
+
+                // Sayaç değerini her seferinde 1 artırarak döngünün sonunda sayaç 10 olduğunda döngünün sona ermesini sağla
+                sayac++;
+            }
+
+            // Programın kapanmaması için bir tuşa basılmasını bekliyoruz
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 2.week_3_while/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace _2.week_3_while
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayac = 0;
+
+            while (sayac < 5) 
+            {
+                Console.WriteLine($"Sayı {sayac}");
+                sayac++;
+            }
+
+
+            string girdi = string.Empty;
+
+            Random rand = new Random();
+
+            int hedef = rand.Next(1,10);
+            int tahmin = 0;
+
+            while (tahmin != hedef)
+            {
+                Console.WriteLine("Bir tahmin yapın");
+                tahmin = int.Parse(Console.ReadLine());
+
+                if(tahmin <  hedef)
+                {
+                    Console.WriteLine("Daha büyük bir sayı girmelesiniz");
+                }
+                else
+                if(tahmin > hedef){
+                    Console.WriteLine("daha hüçük bir sayı girin");
+                }
+                else
+                {
+                    Console.WriteLine("Tebrikler");
+                }
+
+                girdi = Console.ReadLine();
+
+            }
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
