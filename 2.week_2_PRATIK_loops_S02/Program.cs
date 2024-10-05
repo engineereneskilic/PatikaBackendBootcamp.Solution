4df87c98e42215a11c9f70d3c2771459ca473a02
Enes KILIÇ <engineereneskilic@gmail.com>
2024-10-05 21:44:38 +0300

    s02

---
+++ 2.week_2_PRATIK_loops_S02/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _2.week_2_PRATIK_loops_S02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1 ile 20 arasındaki sayıları yazdırmak için bir for döngüsü kullanıyoruz
+            // Döngü 1'den başlayıp 20'ye kadar devam eder (her bir sayıyı tek tek yazdırır)
+            for (int i = 1; i <= 20; i++)
+            {
+                // Mevcut sayıyı ekrana yazdırıyoruz
+                Console.WriteLine(i);
+            }
+
+            // Programın kapanmaması için bir tuşa basılmasını bekliyoruz
+            Console.ReadKey();
+        }
+    }
+}
