fd24ec0c49487eb23ed8e4d7509acf165e2fade9
Enes KILIÇ <engineereneskilic@gmail.com>
2024-10-07 18:46:11 +0300

    methods

---
+++ 2.week_6_Foreach/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _2.week_6_Foreach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayilar = { 4, 8, 6, 7 };
+
+            foreach (var item in sayilar)
+            {
+                Console.WriteLine($"Sayı {item }");
+            }
+        }
+    }
+}
---
+++ 2.week_6_metotlarveparametreler/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime;
+using System.Threading.Channels;
+
+namespace _2.week_6_metotlarveparametreler
+{
+    internal class Program
+    {
+     
+        double OrtalamaHesapla(int[] notlar)
+        {
+            int toplam = 0; 
+
+            for (int i = 0; i < notlar.Length; i++)
+            {
+                toplam += notlar[i];
+            }
+
+            double ortalama = toplam / notlar.Length;
+
+            return ortalama;
+
+        }
+
+      
+    }
+   
+
+   
+}
