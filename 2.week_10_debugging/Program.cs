d7b3c305a9053e1aebedaa4df1291dca0e35db98
Enes KILIÇ <engineereneskilic@gmail.com>
2024-10-08 22:49:57 +0300

    ekleme

---
+++ 2.week_10_debugging/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+
+namespace _2.week_10_debugging
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(Add(i,i));
+            }
+
+            static int Add(int a, int b)
+            {
+                return a + b;
+            }
+        }
+    }
+}
