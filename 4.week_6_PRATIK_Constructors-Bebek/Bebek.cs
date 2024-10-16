dfd4629f015b00a9fafbd6a9ba9414497aba2907
Enes KILIÇ <engineereneskilic@gmail.com>
2024-10-16 20:20:37 +0300

    BebekDogdu

---
+++ 4.week_6_PRATIK_Constructors-Bebek/Bebek.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.week_6_PRATIK_Constructors_Bebek
+{
+    public class Bebek
+    {
+        // Bebek sınıfının property'leri: Ad, Soyad, Doğum Tarihi
+        public string _ad { get; set; }
+        public string _soyad { get; set; }
+        public DateTime _dogumTarihi { get; set; }
+
+        // Default constructor (parametre almayan)
+        public Bebek()
+        {
+            // Her bebek oluşturulduğunda doğum tarihini o anın tarihi olarak atıyoruz
+            _dogumTarihi = DateTime.Now;
+            // Konsola bebek doğduğunda bir "Ingaaaa" sesi yazdırıyoruz
+            Console.WriteLine("Ingaaaa! Bebek doğdu.");
+
+            // Ad ve soyad için varsayılan değerler
+            _ad = "Bilinmiyor";
+            _soyad = "Bilinmiyor";
+        }
+
+        // Alternatif constructor (Ad ve Soyad parametreleri ile)
+        public Bebek(string ad, string soyad)
+        {
+            // Parametre olarak gelen ad ve soyad değerlerini property'lere atıyoruz
+            _ad = ad;
+            _soyad = soyad;
+
+            // Doğum tarihini yine o anın tarihi olarak atıyoruz
+            _dogumTarihi = DateTime.Now;
+
+            // Yine konsola "Ingaaaa" sesi yazdırıyoruz
+            Console.WriteLine("Ingaaaa! Bebek doğdu.");
+        }
+    }
+}
---
+++ 4.week_6_PRATIK_Constructors-Bebek/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _4.week_6_PRATIK_Constructors_Bebek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Parametre almayan default constructor ile bir bebek nesnesi oluşturuyoruz
+            Bebek bebek1 = new Bebek();
+            Console.WriteLine($"Bebek 1: Ad - {bebek1._ad}, Soyad - {bebek1._soyad}, Doğum Tarihi - {bebek1._dogumTarihi}");
+
+            // Ad ve Soyad parametreleri ile alternatif constructor kullanarak bir bebek nesnesi oluşturuyoruz
+            Bebek bebek2 = new Bebek("Ayşe", "Yılmaz");
+            Console.WriteLine($"Bebek 2: Ad - {bebek2._ad}, Soyad - {bebek2._soyad}, Doğum Tarihi - {bebek2._dogumTarihi}");
+
+            Console.ReadKey();
+        }
+    }
+}
