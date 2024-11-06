573faca8fe9d8278c6b552311c1e907a41dd11ac
Enes KILIÇ <engineereneskilic@gmail.com>
2024-11-06 23:50:16 +0300

    Davet Gecesi

---
+++ 5.week_4_List/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _5.week_4_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>() { 3,4,5 };
+
+            List<string> friuts = new List<string>() { "Elma", "Armut" , "Muz" };
+
+            numbers.Add(6);
+            numbers.Insert(2, 10); // 2.indexten sonra 10 ekle
+            numbers.Remove(4);
+
+            numbers.RemoveAt(4);
+            numbers.Contains(3);
+
+            numbers.Clear();
+
+            numbers.Reverse();
+
+            foreach (int item in numbers)
+            {
+                Console.WriteLine(item);
+
+            }
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                Console.WriteLine(numbers[i]);
+
+            }
+
+
+
+        }
+    }
+}
---
+++ 5.week_5_PRATIK_DavetGecesi_Lists/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _5.week_5_PRATIK_DavetGecesi_Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ziyaretçilerin isimlerini içeren listeyi tanımlıyoruz
+            List<string> guestList = new List<string>
+            {
+                "Bülent Ersoy",
+                "Ajda Pekkan",
+                "Ebru Gündeş",
+                "Hadise",
+                "Hande Yener",
+                "Tarkan",
+                "Funda Arar",
+                "Demet Akalın"
+            };
+
+            // Gala gecesi başlığı
+            Console.WriteLine("Patika Plus Gala Gecesi Ziyaretçi Listesi:");
+
+            // foreach döngüsü kullanarak liste içindeki isimleri ekrana yazdırıyoruz
+            foreach (string guest in guestList)
+            {
+                Console.WriteLine("- " + guest); // Her bir ismi başına "-" ekleyerek yazdırıyoruz
+            }
+
+        }
+    }
+}
