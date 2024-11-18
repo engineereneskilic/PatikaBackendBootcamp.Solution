67c76d63563a26a14ee96ad4aa80ce514a544b37
Enes KILIÇ <engineereneskilic@gmail.com>
2024-11-18 23:43:45 +0300

    ASYNC AWAIT

---
+++ 7.week_8_AsyncAwait/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace _7.week_8_AsyncAwait
+{
+    internal class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            PerformOperation();
+
+           string result = await FetchData();
+
+           await Console.Out.WriteLineAsync(result);
+
+        }
+
+        static async Task<string> FetchData()
+        {
+            HttpClient client = new HttpClient();
+            string result = await client.GetStringAsync("http://url.com");
+
+            return result;
+
+        }
+
+        static async Task PerformOperation()
+        {
+
+            await Task.Delay(3000);
+
+            await Console.Out.WriteLineAsync("ASYNC OOP");
+
+        }
+
+    }
+}
